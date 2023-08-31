8a8e268c33e9716fa09e2fe4519fc16dd0e100ab
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-08-31 22:16:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 import os
-from transformers import AutoTokenizer, pipeline, logging
-from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, logging
+import torch
 import argparse
+from huggingface_hub import snapshot_download
 
-model_name_or_path = "TheBloke/vicuna-7B-v1.3-GPTQ"
-model_basename = "vicuna-7b-v1.3-GPTQ-4bit-128g.no-act.order"
-os.environ["TOKENIZERS_PARALLELISM"] = "true"
+model_name_or_path = "lmsys/vicuna-13b-v1.1"
 
 class InferlessPythonModel:
     def initialize(self):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self.model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
-            model_basename=model_basename,                                            
-            use_safetensors=True,
-            trust_remote_code=True,
-            device_map='auto',
-            use_triton=True,
-            quantize_config=None
-        )
+        self.model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
 
 
     def infer(self, inputs):
